c68d56b1fa15e666a2fabb42382958a6409c8e8f
mlshere <mlsherezade@gmail.com>
2023-08-29 16:56:24 +0200

    style menu + scroll to top javascript

--- js/script.js
+++ js/script.js
@@ -8,4 +8,21 @@ function menuToggle() {
       x.className = 'navtoggle';
     }
   }
-  
+
+
+
+  $(document).ready(function () 
+  {
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
+        $('.scroll-to-top').fadeIn();
+      } else {
+        $('.scroll-to-top').fadeOut();
+      }
+    })
+
+    ('#scroll-to-top-btn').click(function () {
+      $('html, body').animate({ scrollTop: 0 }, 800);
+      return false;
+    })
+});
